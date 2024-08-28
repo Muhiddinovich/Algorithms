392d66baf027aa171e9139ca389e454220a7ab85
mullagoziev@gmail.com <Fahriddin@WIN-U2794NDE3MO>
2024-08-28 06:15:31 -0400

    Quick Union

--- helloWorld/src/main/java/dynamicConnectivityProblem/QuickUnionUF.java
+++ helloWorld/src/main/java/dynamicConnectivityProblem/QuickUnionUF.java
@@ -3,25 +3,26 @@ package dynamicConnectivityProblem;
 public class QuickUnionUF {
 	private int[] id;
 
-	public QuickUnionUF(int N) {
+	public QuickUnionUF(int N) { // set id of each object to itself (N array accessed)
 		id = new int[N];
 		for (int i = 0; i < N; i++) {
 			id[i] = i;
 		}
 	}
 
-	private int root(int i) {
+	private int root(int i) { // chase parent pointers until reach root (depth of array accesses)
+
 		while (i != id[i]) {
 			i = id[i];
 		}
 		return i;
 	}
 
-	public boolean connected(int p, int q) {
+	public boolean connected(int p, int q) { // check if p and q have same root
 		return root(p) == root(q);
 	}
 
-	public void union(int p, int q) {
+	public void union(int p, int q) { // change root of p to point to root of q
 		int i = root(p);
 		int j = root(q);
 		id[i] = j;
